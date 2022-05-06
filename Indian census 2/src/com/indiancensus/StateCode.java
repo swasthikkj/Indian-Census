ef69f564355327c40056b6b27f5474fc109e47c8
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-05-06 08:06:45 +0530

    program

---
+++ Indian census 2/src/com/indiancensus/StateCode.java
@@ -0,0 +1,47 @@
+package com.indiancensus;
+import com.opencsv.bean.CsvBindByName;
+
+public class StateCode {
+	    @CsvBindByName(column = "SrNo")
+	    private String serialNo;
+	    @CsvBindByName(column = "stateName")
+	    private String stateName;
+	    @CsvBindByName(column = "tin")
+	    private Integer tin;
+	    @CsvBindByName(column = "stateCode")
+	    private String stateCode;
+	    public String getSerialNo() {
+	        return serialNo;
+	    }
+	    public void setSerialNo(String serialNo) {
+	        this.serialNo = serialNo;
+	    }
+	    public String getStateName() {
+	        return stateName;
+	    }
+	    public void setStateName(String stateName) {
+	        this.stateName = stateName;
+	    }
+	    public Integer getTin() {
+	        return tin;
+	    }
+	    public void setTin(Integer tin) {
+	        this.tin = tin;
+	    }
+	    public String getStateCode() {
+	        return stateCode;
+	    }
+	    public void setStateCode(String stateCode) {
+	        this.stateCode = stateCode;
+	    }
+	    @Override
+	    public String toString() {
+	        return "StateCode{" +
+	                "serialNo='" + serialNo + '\'' +
+	                ", stateName='" + stateName + '\'' +
+	                ", tin=" + tin +
+	                ", stateCode='" + stateCode + '\'' +
+	                '}';
+	    }
+		    
+}
